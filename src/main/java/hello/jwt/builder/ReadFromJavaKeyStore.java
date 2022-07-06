cad9a09a0edf15143fd9018326dcb3a326f68e35
julsarik <julien.sarik@finastra.com>
2022-07-06 10:10:21 +0200

    refactor

--- src/main/java/hello/jwt/builder/ReadFromJavaKeyStore.java
+++ src/main/java/hello/jwt/builder/ReadFromJavaKeyStore.java
@@ -9,13 +9,15 @@ import org.springframework.security.jwt.crypto.sign.MacSigner;
 import org.springframework.security.jwt.crypto.sign.RsaSigner;
 import org.springframework.security.jwt.crypto.sign.Signer;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.Key;
 import java.security.KeyStore;
 import java.security.interfaces.RSAPrivateKey;
 import java.util.Collections;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class ReadFromJavaKeyStore {
@@ -29,7 +31,7 @@ public class ReadFromJavaKeyStore {
         final String alias = "foo-client";
         final String keyPass = "p1";
         final KeyStore store = KeyStore.getInstance("JKS");
-        final InputStream input = new FileInputStream(fileName);
+        final InputStream input = Files.newInputStream(Paths.get(fileName));
         store.load(input, storePass);
 //        final Certificate entry = store.getCertificate(alias);
         final Key key = store.getKey(alias, keyPass.toCharArray());
@@ -42,10 +44,12 @@ public class ReadFromJavaKeyStore {
         content.put("sub", client_id);
         content.put("aud", "https://api.preprod.fusionfabric.cloud/login/v1");
         content.put("exp", System.currentTimeMillis() / 1000 + TimeUnit.HOURS.toSeconds(1));
-        content.put("jti", Long.toString(System.currentTimeMillis()));
+        content.put("jti", UUID.randomUUID().toString());
+
         final Map<String, String> header = Collections.singletonMap("kid", "-RtrYV6X0U5WkXBGjbXDlb2APg8vkWY_hhFjQey3mrY");
 
         final Jwt jwt = createJwt(content, header, signer);
+
         final String jwtString = jwt.getEncoded();
         System.out.println(jwtString);
     }
--- src/main/java/hello/jwt/builder/ReadFromPem.java
+++ src/main/java/hello/jwt/builder/ReadFromPem.java
@@ -8,16 +8,17 @@ import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.springframework.security.jwt.Jwt;
 import org.springframework.security.jwt.JwtHelper;
-import org.springframework.security.jwt.crypto.sign.MacSigner;
 import org.springframework.security.jwt.crypto.sign.RsaSigner;
 import org.springframework.security.jwt.crypto.sign.Signer;
 
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.KeyPair;
 import java.security.interfaces.RSAPrivateKey;
 import java.util.Collections;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class ReadFromPem {
@@ -27,35 +28,27 @@ public class ReadFromPem {
 
     public static void main(String[] args) throws Exception {
         // read PEM file
-        PEMParser pemParser = new PEMParser(new InputStreamReader(new FileInputStream(KEY_FILE)));
-        PEMKeyPair pemKeyPair = (PEMKeyPair)pemParser.readObject();
+        final PEMParser pemParser = new PEMParser(new InputStreamReader(Files.newInputStream(Paths.get(KEY_FILE))));
         // Convert to Java (JCA) format
-        JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
-        KeyPair keyPair = converter.getKeyPair(pemKeyPair);
+        final KeyPair keyPair = new JcaPEMKeyConverter().getKeyPair((PEMKeyPair) pemParser.readObject());
         pemParser.close();
-
-        final Signer signer = rsaSigner((RSAPrivateKey) keyPair.getPrivate());
-
+        // create signer from private key
+        final Signer signer = new RsaSigner((RSAPrivateKey) keyPair.getPrivate(), "SHA256withRSA");
+        // build JWT payload
         final ObjectNode content = MAPPER.createObjectNode();
         final String client_id = "45d8d6b6-de7c-449f-b7f7-b4e05ced432d";
         content.put("iss", client_id);
         content.put("sub", client_id);
         content.put("aud", "https://api.fusionfabric.cloud/login/v1");
         content.put("exp", System.currentTimeMillis() / 1000 + TimeUnit.HOURS.toSeconds(1));
-        content.put("jti", Long.toString(System.currentTimeMillis()));
+        content.put("jti", UUID.randomUUID().toString());
+
         final Map<String, String> header = Collections.singletonMap("kid", "-RtrYV6X0U5WkXBGjbXDlb2APg8vkWY_hhFjQey3mrY");
 
         final Jwt jwt = createJwt(content, header, signer);
+
         final String jwtString = jwt.getEncoded();
         System.out.println(jwtString);
-    }
-
-    private static Signer rsaSigner(RSAPrivateKey privateKey) {
-        return new RsaSigner(privateKey, "SHA256withRSA");
-    }
-
-    private static Signer macSigner(String secret) {
-        return new MacSigner(secret);
     }
 
     private static Jwt createJwt(ObjectNode content, Map<String, String> jwtHeader, Signer signer) {
